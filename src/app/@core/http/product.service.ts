172703e464fcda33238e2211d27caf4c65c8e8d5
Jez Reel Maghuyop <jez@sharken.io>
2024-03-24 07:47:28 +0100

    implement products services and page display

--- src/app/@core/http/product.service.ts
+++ src/app/@core/http/product.service.ts
@@ -1,16 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Product } from '../models';
+import { Observable, map } from 'rxjs';
+import { Response, Product } from '../models';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
   private readonly http: HttpClient = inject(HttpClient);
 
-  getAll() : Observable<Product[]> {
-    return this.http.get<Product[]>('/data-api/rest/products');
+  getAll(): Observable<Product[]> {
+    return this.http
+      .get<Response<Product[]>>('/data-api/rest/products')
+      .pipe(map((response) => response.value));
+  }
+
+  get(id: number): Observable<Product> {
+    return this.http
+      .get<Response<Product[]>>(`/data-api/rest/products/Id/${id}`)
+      .pipe(map((response) => response.value[0]));
+  }
+
+  filter(take: number = 100, orderBy: string = 'Id', order: 'asc' | 'desc' = 'asc'): Observable<Product[]> {
+    return this.http
+      .get<Response<Product[]>>(`/data-api/rest/products?$first=${take}&$orderby=${orderBy} ${order}`)
+      .pipe(map((response) => response.value));
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,44 @@ import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
 import { CartService, ProductService } from './@core';
+import { HttpClientModule } from '@angular/common/http';
+import { Product } from './@core/models';
+
+type Context = {
+  products: Product[];
+};
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationComponent],
+  imports: [RouterOutlet, NavigationComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  providers: [ProductService, CartService],
 })
 export class AppComponent implements OnInit {
   private readonly productService: ProductService = inject(ProductService);
   private readonly cartService: CartService = inject(CartService);
 
+  context: Context = {
+    products: [],
+  };
+
   ngOnInit(): void {
-    this.productService.getAll().subscribe((products) => {
-      console.log(products);
+    // this.productService.getAll().subscribe((products) => {
+    //   this.context.products = products;
+    // });
+    
+    // this.productService.filter(5).subscribe((products) => {
+    //   this.context.products = products;
+    // });
+
+    this.productService.filter(5, 'Name', 'desc').subscribe((products) => {
+      this.context.products = products;
+    });
+
+     this.productService.get(6).subscribe((product) => {
+      console.log(product);
     });
   }
 }
